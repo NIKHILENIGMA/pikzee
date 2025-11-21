2e798450ef5b20b06c39b11aef639028eea54b66
NIKHILENIGMA <nickharmalkar18@gmail.com>
2025-11-21 03:47:25 -0800

    refactor: simplify User interface and remove unused types from Request extension

--- server/src/types/globals.d.ts
+++ server/src/types/globals.d.ts
@@ -2,47 +2,14 @@
 import 'express-serve-static-core'
 interface User {
     id: string
-    email: string | null
-    tierId: number
-    createdAt: Date
 }
-
-interface Tiers {
-    name: 'FREE' | 'PRO' | 'ENTERPRISE'
-    storageLimitBytes: number
-    fileUploadLimitBytes: number
-    membersPerWorkspaceLimit: number
-    projectsPerWorkspaceLimit: number
-    docsPerWorkspaceLimit: number
-    draftsPerDocLimit: number
-}
-
-interface Workspaces {
-    id: number
-    name: string
-    ownerId: string
-    createdAt: Date
-}
-
-import { Project } from '@/modules/projects/project.types'
 
 // Extend the Request interface to include auth property
 declare global {
     namespace Express {
         export interface Request {
             user?: User
-            tier?: Tiers
-            workspace?: Workspaces
-            project?: Project
         }
     }
 }
 
-// import 'express'; // Important: This imports the original 'express' module
-
-// declare module 'express-serve-static-core' {
-//   interface Request {
-//     customValue?: string; // Define your custom property and its type
-//     // You can add more custom properties here
-//   }
-// }
