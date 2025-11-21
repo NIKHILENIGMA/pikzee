8f0dc352c0ac05039a1c97d334b83da251ec09f1
NIKHILENIGMA <nickharmalkar18@gmail.com>
2025-11-21 03:46:48 -0800

    refactor: remove project management module and related validation schemas

--- server/src/shared/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './services/tier.service'
-export * from './services/validation.service'
-
-export * from './validators/workspace.validator'
-export * from './validators/project.validators'
-export * from './validators/workspace-members.validator'
