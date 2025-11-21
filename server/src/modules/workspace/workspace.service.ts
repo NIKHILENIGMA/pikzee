d8c56f5603044a547a702a82034778bb450b8906
NIKHILENIGMA <nickharmalkar18@gmail.com>
2025-11-21 07:09:35 -0800

    refactor: change WorkspaceService class to export for improved accessibility

--- server/src/modules/workspace/workspace.service.ts
+++ server/src/modules/workspace/workspace.service.ts
@@ -22,7 +22,7 @@ type MemberMap = {
     avatarUrl: string | null
 }
 
-class WorkspaceService {
+export class WorkspaceService {
     private static instance: WorkspaceService
 
     constructor() {}
