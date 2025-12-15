afe1c637cab6becc7dced7ad63aa57008579dffe
NIKHILENIGMA <nickharmalkar18@gmail.com>
2025-12-15 07:37:26 -0800

    feat: Remove unused notification interfaces and enums to simplify notification types

--- server/src/core/notification/notification.types.ts
+++ server/src/core/notification/notification.types.ts
@@ -34,36 +34,3 @@ export interface SendInvitationDTO {
     rejectLink: string
     customMessage: string
 }
-
-export interface CreateNotificationSubscriber {
-    subscriberId: string
-    email?: string
-    firstName?: string
-    lastName?: string
-    avatar?: string
-}
-
-export interface NotificationTrigger<T> {
-    subscriberId: string
-    workflowId: string
-    type: NotificationChannel
-    to?: {
-        email?: string
-        firstName?: string
-        lastName?: string
-    }
-    payload: T
-}
-
-export enum NotificationChannel {
-    EMAIL = 'email',
-    IN_APP = 'in_app'
-}
-
-export interface WorkflowConfig {
-    id: string
-    name: string
-    description?: string
-    tags?: string[]
-    channels: NotificationChannel[]
-}
