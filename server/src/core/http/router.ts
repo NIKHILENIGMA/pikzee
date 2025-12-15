be721d74d26faed249fa72c8570ca700bd41e2b6
NIKHILENIGMA <nickharmalkar18@gmail.com>
2025-12-15 07:32:12 -0800

    feat: Add invitation routes to the main router

--- server/src/core/http/router.ts
+++ server/src/core/http/router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 
-import { workspace, admin } from '@/modules'
+import { workspace, admin, invitation } from '@/modules'
 
 const router = Router()
 
+router.use('/invitations', invitation.invitationRouter)
 router.use('/workspaces', workspace.workspaceRouter)
 router.use('/admin', admin.adminRouter)
 
