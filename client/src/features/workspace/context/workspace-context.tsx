ebc1496f427ea8b6db62febf1092ff3e9cd7c521
NIKHILENIGMA <nickharmalkar18@gmail.com>
2025-12-24 22:55:33 -0800

    feat: Add WorkspaceProvider component to manage workspace context

---
+++ client/src/features/workspace/context/workspace-context.tsx
@@ -0,0 +1,34 @@
+import { type FC, type ReactNode } from 'react'
+
+import { WorkspaceContext } from '../hooks/useWorkspaceContext'
+import type { Project, WorkspaceDTO } from '../types'
+
+type WorkspaceContextType = {
+    id: string
+    name: string
+    logoUrl: string | null
+    projects?: Project[]
+}
+
+const initialWorkspaceState: WorkspaceContextType = {
+    id: '',
+    name: '',
+    logoUrl: null,
+    projects: []
+}
+
+interface WorkspaceProviderProps {
+    workspace: WorkspaceDTO | null
+    children: ReactNode
+}
+
+export const WorkspaceProvider: FC<WorkspaceProviderProps> = ({ workspace, children }) => {
+    const value = {
+        id: workspace?.id ?? initialWorkspaceState.id,
+        name: workspace?.name ?? initialWorkspaceState.name,
+        logoUrl: workspace?.logoUrl ?? initialWorkspaceState.logoUrl,
+        projects: workspace?.projects ?? initialWorkspaceState.projects
+    }
+
+    return <WorkspaceContext.Provider value={value}>{children}</WorkspaceContext.Provider>
+}
