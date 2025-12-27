59a708f2b1219394ad310a73c762a8bed040cd92
NIKHILENIGMA <nickharmalkar18@gmail.com>
2025-12-27 10:41:52 -0800

    feat: Enhance ProjectOptions component with loading state and improved UI elements

--- client/src/features/workspace/components/project/project-options.tsx
+++ client/src/features/workspace/components/project/project-options.tsx
@@ -1,4 +1,4 @@
-import { Cog, ShieldBan, Trash2 } from 'lucide-react'
+import { Cog, Loader, Settings2Icon, ShieldBan, Trash2 } from 'lucide-react'
 import { type FC, type ReactNode } from 'react'
 
 import { Button } from '@/components/ui/button'
@@ -9,6 +9,7 @@ import { useDeleteProject } from '../../api/project/delete-project'
 import { useUpdateProjectStatus } from '../../api/project/update-project-status'
 import type { ProjectStatus } from '../../types'
 import ProjectAccessDialog from '../project/project-access-dialog'
+import { toast } from 'sonner'
 
 const ProjectOptions: FC<{ children: ReactNode; projectId: string; status: ProjectStatus }> = ({ children, projectId, status }) => {
     const updateProjectStatusMutation = useUpdateProjectStatus()
@@ -20,10 +21,12 @@ const ProjectOptions: FC<{ children: ReactNode; projectId: string; status: Proje
             projectId,
             status: newStatus
         })
+        toast.success(`Project marked as ${newStatus.toLowerCase()}`)
     }
 
     const handleProjectDeletion = async (id: string) => {
         await deleteProjectMutation.mutateAsync(id)
+        toast.success('Project deleted successfully')
     }
 
     return (
@@ -35,22 +38,30 @@ const ProjectOptions: FC<{ children: ReactNode; projectId: string; status: Proje
                 key={projectId}>
                 <div className="flex flex-col space-y-1.5">
                     <div className="border-b border-border pb-2.5">
-                        <p className="text-sm text-start p-1.5 font-medium text-foreground/70">Project Options</p>
+                        <p className="text-sm text-start px-2.5 pt-0.5 font-medium text-foreground/70">Project Options</p>
                     </div>
+                    <Button
+                        variant={'ghost'}
+                        size={'sm'}
+                        className="text-start hover:bg-secondary rounded-sm justify-start gap-2">
+                        <>
+                            <Settings2Icon className="w-4 h-4" /> Project Settings
+                        </>
+                    </Button>
                     <ProjectAccessDialog />
                     <Button
                         variant={'ghost'}
                         size={'sm'}
-                        className="text-start hover:bg-secondary rounded-sm justify-start"
+                        className="text-start hover:bg-secondary rounded-sm justify-start gap-2"
                         onClick={handleStatusChange}>
                         {status === 'ACTIVE' ? (
-                            <span>
-                                <Cog /> Mark Inactive
-                            </span>
+                            <>
+                                <Cog className="w-4 h-4" /> Mark Inactive
+                            </>
                         ) : (
-                            <span>
-                                <ShieldBan /> Mark Active
-                            </span>
+                            <>
+                                <ShieldBan className="w-4 h-4" /> Mark Active
+                            </>
                         )}
                     </Button>
                     <Separator />
@@ -58,7 +69,15 @@ const ProjectOptions: FC<{ children: ReactNode; projectId: string; status: Proje
                         size={'sm'}
                         onClick={() => handleProjectDeletion(projectId)}
                         className="w-full text-start border border-destructive/30 rounded-lg bg-destructive/20 text-foreground justify-start hover:bg-destructive hover:text-white">
-                        <Trash2 /> <span className="text-start">Delete Project</span>
+                        {deleteProjectMutation.isPending ? (
+                            <>
+                                <Loader /> Deleting...
+                            </>
+                        ) : (
+                            <>
+                                <Trash2 className="w-4 h-4" /> Delete Project
+                            </>
+                        )}
                     </Button>
                 </div>
             </PopoverContent>
