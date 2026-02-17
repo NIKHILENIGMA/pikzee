77ad908d2a88474fa58f12efdb57b090d3782bc7
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-02-17 08:59:37 -0800

    refactor: remove unused Google client implementation to streamline codebase

--- server/src/core/storage/google.client.ts
+++
@@ -1,9 +0,0 @@
-import { google } from 'googleapis'
-
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI } from '@/config'
-
-export const oauth2Client: InstanceType<typeof google.auth.OAuth2> = new google.auth.OAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    GOOGLE_REDIRECT_URI
-)
