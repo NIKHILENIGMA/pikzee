1e810a7afc532b5c1d743ee7d311322db2d7793f
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-03-16 08:17:56 -0700

    feat: update createDraft function to include documentId for draft creation

--- client/src/features/drafts/api/create-draft.ts
+++ client/src/features/drafts/api/create-draft.ts
@@ -7,17 +7,18 @@ import { draftKeys } from '@/shared/lib/query-keys'
 
 import type { DraftDTO } from '../types/draft.types'
 
-export const createDraft = async (data: { workspaceId: string }) => {
-    const newDocument = await client.post<DraftDTO, null>(`${DOCUMENT_API_BASE}?workspaceId=${data.workspaceId}`, null)
+export const createDraft = async (data: { documentId: string; workspaceId: string }) => {
+    const newDocument = await client.post<DraftDTO, null>(`${DOCUMENT_API_BASE}/${data.documentId}/drafts?workspaceId=${data.workspaceId}`, null)
 
     return newDocument.data
 }
 
-type UseCreateDocument = {
+
+type UseCreateDraft = {
     mutationConfig?: MutationConfig<typeof createDraft>
 }
 
-export const useCreateDocument = ({ workspaceId, mutationConfig }: UseCreateDocument & { workspaceId: string }) => {
+export const useCreateDraft = ({ workspaceId, mutationConfig }: UseCreateDraft & { workspaceId: string }) => {
     const queryClient = useQueryClient()
     const { ...restConfig } = mutationConfig || {}
 
