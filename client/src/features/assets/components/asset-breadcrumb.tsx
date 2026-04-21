fb52bc35443cbed60b8fe6633a5110cc3956f04e
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-04-21 10:45:08 -0700

    feat: enhance AssetBreadcrumb component with navigation improvements and styling adjustments

--- client/src/features/assets/components/asset-breadcrumb.tsx
+++ client/src/features/assets/components/asset-breadcrumb.tsx
@@ -1,5 +1,6 @@
 import { ChevronRightIcon, Folder } from 'lucide-react'
 import { Fragment, type FC } from 'react'
+import { useNavigate } from 'react-router'
 
 interface AssetBreadcrumbProps {
     breadcrumb: {
@@ -10,13 +11,18 @@ interface AssetBreadcrumbProps {
 }
 
 const AssetBreadcrumb: FC<AssetBreadcrumbProps> = ({ breadcrumb, navigateToFolder }) => {
+    const navigate = useNavigate()
     return (
         <header className=" border-b border-secondary px-6 py-4">
-            <h3 className="pb-4">Assets</h3>
+            <div className="mb-2 flex items-center space-x-2">
+                <h3 className="pb-4 font-semibold text-lg cursor-pointer" onClick={() => navigate('/ws')}>
+                    Home
+                </h3>
+            </div>
             <nav className="flex items-center space-x-2 text-sm text-foreground/85 mb-2">
                 <button
                     onClick={() => navigateToFolder(null)}
-                    className="text-black dark:text-white font-medium transition-colors">
+                    className="text-black dark:text-white font-medium transition-colors cursor-pointer">
                     <Folder />
                 </button>
 
@@ -25,7 +31,7 @@ const AssetBreadcrumb: FC<AssetBreadcrumbProps> = ({ breadcrumb, navigateToFolde
                         <ChevronRightIcon className="w-4 h-4 text-foreground/85" />
                         <button
                             onClick={() => navigateToFolder(crumb.id)}
-                            className="hover:text-primary font-medium transition-colors">
+                            className="hover:text-primary font-medium transition-colors cursor-pointer">
                             {crumb.name}
                         </button>
                     </Fragment>
